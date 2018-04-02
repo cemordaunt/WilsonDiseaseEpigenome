a21f164413ed99044b2248d0b23639aa919312b5
Charles Mordaunt <cemordaunt@ucdavis.edu>
2018-04-02 16:53:21 -0700

    Add files via upload

---
+++ Liver/Bash/DMRfinder_head.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#SBATCH --workdir /share/lasallelab/Charles/DK_WGBS_HumanLiver
+#SBATCH --mem=100000
+#SBATCH --time=2-0:00
+#SBATCH -n 2
+#SBATCH --partition=gc,gc64,gc128,gc256,gc512
---
+++ Liver/Bash/DMRmethyl_head.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#SBATCH --workdir /share/lasallelab/Charles/DK_WGBS_HumanLiver
+#SBATCH --mem=50000
+#SBATCH --time=1-0:00
+#SBATCH -n 2
+#SBATCH --partition=gc,gc64,gc128,gc256,gc512
